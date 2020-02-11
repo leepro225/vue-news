13025a0d342bc1f88151a0006d152c4ab814ae7a
ha-co <leepro225@gmail.com>
2020-02-11 22:21:17 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ToolBar></ToolBar>
+    <router-view></router-view>
+  </div>
+  <!-- test -->
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue'
+
+export default {
+  components: {
+    ToolBar,
+  }
+}
+</script>
+
+<style>
+</style>
