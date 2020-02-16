bbaca0b569b0572ffa46cfc53a23fe0deed75ab4
ha-co <leepro225@gmail.com>
2020-02-16 21:50:42 +0900

    api

--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -1,9 +1,22 @@
 <template>
-    <div>
-        <ul>
-            <li><router-link to="/news">News</router-link></li>
-            <li><router-link to="/ask">Ask</router-link></li>
-            <li><router-link to="/jobs">Jobs</router-link></li>
-        </ul>
+    <div class="header">
+        <router-link to="/news">News</router-link> |
+        <router-link to="/ask">Ask</router-link> |
+        <router-link to="/jobs">Jobs</router-link>
     </div>
 </template>
+
+<style scoped>
+.header {
+    color: white;
+    background: #42b883;
+    display: flex;
+    padding:8px;
+}
+.header .router-link-exact-active {
+    color: #35495e;
+}
+.header a {
+    color:white;
+}
+</style>
