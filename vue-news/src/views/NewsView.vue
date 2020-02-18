c604bbbd5956501d91682a33a5f6cfbade13318e
ha-co <leepro225@gmail.com>
2020-02-18 20:34:07 +0900

    api

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,3 +1,18 @@
 <template>
     <div>news</div>
 </template>
+
+<script>
+import {fetchNewsList} from '../api/index'
+export default {
+    created() {
+        fetchNewsList()
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+}
+</script>
