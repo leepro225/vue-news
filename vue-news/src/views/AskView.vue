0b94cea1f89db9091cec93cbbac411f5e3a04216
ha-co <leepro225@gmail.com>
2020-02-20 22:14:13 +0900

    jobs and ask api

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,3 +1,25 @@
 <template>
-    <div>ask</div>
+    <div>
+        <div v-for="item in ask" v-bind:key="item.id">{{ item.title }}</div>
+    </div>
 </template>
+
+<script>
+import {fetchAskList} from '../api/index'
+export default {
+    data() {
+      return {
+          ask: []
+      }  
+    },
+    created() {
+        fetchAskList()
+        .then(res => {
+            this.ask = res.data;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+}
+</script>
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,3 +1,24 @@
 <template>
-    <div>jobs</div>
+    <div>
+        <div v-for="job in jobs" v-bind:key="job.id">{{ job.title }}</div>
+    </div>
 </template>
+<script>
+import {fetchJobsList} from '../api/index'
+export default {
+    data() {
+        return {
+           jobs : [] 
+        }
+    },
+    created() {
+        fetchJobsList()
+            .then(res => {
+                this.jobs = res.data;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
+</script>
