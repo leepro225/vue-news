6a5fde879d957152c120e4968fb99a492e2fd679
ha-co <leepro225@gmail.com>
2020-02-26 23:49:24 +0900

    router

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <p v-for="user in this.$store.state.news" v-bind:key="user.id">
-            <a :href="user.url">{{ user.title }}</a>   <small style="color:#aaa5a5;">{{user.time_ago}} by {{user.user}}</small></p>
+            <a :href="user.url">{{ user.title }}</a>   <small style="color:#aaa5a5;">{{user.time_ago}} 
+                by <router-link to="`/user/${user.user}`">{{user.user}}</router-link></small></p>
    </div>
 </template>
 
